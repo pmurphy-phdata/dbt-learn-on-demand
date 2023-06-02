a489003ed9f3b3960b6634863677e06719bd4795
pmurphy@phdata.io <pmurphy@phdata.io>
2023-06-02 20:37:19 +0000

    more tests

--- models/marts/core/date_spine.sql
+++ models/marts/core/date_spine.sql
@@ -1,9 +1,9 @@
 {{
-    config(
-        materialized='table'
+     config(
+          materialized='table'
+         , required_tests=None
     )
 }}
-
 {{ dbt_utils.date_spine(
     datepart="day",
     start_date="cast('2019-01-01' as date)",
